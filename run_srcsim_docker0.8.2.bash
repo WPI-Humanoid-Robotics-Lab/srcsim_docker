594f665e8ccd61927f40dccc8704a37113ae4357
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2018-03-20 17:08:47 -0400

    Fixed Issue #2. Added local build of docker image

--- run_srcsim_docker0.8.2.bash
+++ run_srcsim_docker0.8.2.bash
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Pulling docker image froim docker hub"
-docker pull ninja777/srcsim:0.8.2
+if [[ "$(docker images -q srcsim:0.8.2 2> /dev/null)" == "" ]]; then
+  git clone -b 0.8.2 https://github.com/WPI-Humanoid-Robotics-Lab/srcsim_docker.git
+  cd srcsim_docker
+  docker build -t srcsim:0.8.2 .
+  cd .. && rm -rf srcsim_docker
+fi
+
+if [[ "$(docker network ls | grep srcsim 2> /dev/null)" == "" ]]; then
+  echo "creating network bridge for docker image"
+  docker network create --subnet 201.1.1.0/16 --driver bridge srcsim
+fi
 
 echo "running srcsim 0.8.2 docker container"
 
@@ -46,14 +55,14 @@ DISPLAY="${DISPLAY:-:0}"
 docker run --rm --name srcsim \
   -e DISPLAY=unix$DISPLAY \
   -e XAUTHORITY=/tmp/.docker.xauth \
-  -e ROS_MASTER_URI=http://192.168.2.10:11311 \
-  -e ROS_IP=192.168.2.10 \
+  -e ROS_MASTER_URI=http://201.1.1.10:11311 \
+  -e ROS_IP=201.1.1.10 \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "/tmp/.docker.xauth:/tmp/.docker.xauth" \
   -v /dev/log:/dev/log \
   --ulimit rtprio=99 \
   --net=srcsim \
-  --ip=192.168.2.10 \
+  --ip=201.1.1.10 \
   ${DOCKER_GPU_PARAMS} \
-  ninja777/srcsim:0.8.2
+  srcsim:0.8.2
--- run_srcsim_docker0.9.0.bash
+++ run_srcsim_docker0.9.0.bash
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-echo "Pulling docker image froim docker hub"
-docker pull ninja777/srcsim:0.9.0
+if [[ "$(docker images -q srcsim:0.9.0 2> /dev/null)" == "" ]]; then
+  git clone -b 0.9.0 https://github.com/WPI-Humanoid-Robotics-Lab/srcsim_docker.git
+  cd srcsim_docker
+  docker build -t srcsim:0.9.0 .
+  cd .. && rm -rf srcsim_docker
+fi
 
-echo "running srcsim 0.9 docker container"
+if [[ "$(docker network ls | grep srcsim 2> /dev/null)" == "" ]]; then
+  echo "creating network bridge for docker image"
+  docker network create --subnet 201.1.1.0/16 --driver bridge srcsim
+fi
+
+echo "running srcsim 0.9.0 docker container"
 
 # XAUTH=/tmp/.docker.xauth
 # xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
@@ -45,14 +54,14 @@ DISPLAY="${DISPLAY:-:0}"
 docker run --rm --name srcsim \
   -e DISPLAY=unix$DISPLAY \
   -e XAUTHORITY=/tmp/.docker.xauth \
-  -e ROS_MASTER_URI=http://192.168.2.10:11311 \
-  -e ROS_IP=192.168.2.10 \
+  -e ROS_MASTER_URI=http://201.1.1.10:11311 \
+  -e ROS_IP=201.1.1.10 \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "/tmp/.docker.xauth:/tmp/.docker.xauth" \
   -v /dev/log:/dev/log \
   --ulimit rtprio=99 \
   --net=srcsim \
-  --ip=192.168.2.10 \
+  --ip=201.1.1.10 \
   ${DOCKER_GPU_PARAMS} \
-  ninja777/srcsim:0.9.0
+  srcsim:0.9.0
